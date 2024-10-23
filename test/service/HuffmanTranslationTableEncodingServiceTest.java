6a1f77cb7f8ba0eb9fe0d6b331b98efedd9699bf
fDero <francescodero@outlook.it>
2024-10-23 14:42:54 +0200

    Add more tests

--- test/service/HuffmanTranslationTableEncodingServiceTest.java
+++ test/service/HuffmanTranslationTableEncodingServiceTest.java
@@ -1,39 +1,64 @@
 package service;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.util.Map;
-import java.util.PriorityQueue;
-
 import org.junit.jupiter.api.Test;
 
 import model.BitSequence;
-import model.HuffmanTranslationTable;
+import model.SymbolTable;
 
-class HuffmanTranslationTableEncodingServiceTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-    private final HuffmanTranslationTableEncodingService huffmanTranslationTableEncodingService 
-        = new HuffmanTranslationTableEncodingService();
+import java.util.Map;
+import java.util.HashMap;
+import java.util.PriorityQueue;
 
-    private byte characterA = (byte) 'a';
-    private byte characterB = (byte) 'b';
-    private byte characterC = (byte) 'c';
-    private byte characterD = (byte) 'd';
+public class HuffmanTranslationTableEncodingServiceTest {
+    
+    private final HuffmanTranslationTableGenerationService huffmanTranslationTableGenerationService 
+        = new HuffmanTranslationTableGenerationService();
 
-    private final HuffmanTranslationTable translationTable = new HuffmanTranslationTable(Map.of(
-        characterA, new BitSequence("0"), 
-        characterB, new BitSequence("10"), 
-        characterC, new BitSequence("110"), 
-        characterD, new BitSequence("111")
+    private int characterA = (int) 'a';
+    private int characterB = (int) 'b';
+    private int characterC = (int) 'c';
+    private int characterD = (int) 'd';
+
+    private final SymbolTable exampleSymbolTable = new SymbolTable(Map.of(
+        characterA, 3L, 
+        characterB, 2L, 
+        characterC, 1L, 
+        characterD, 1L
     ));
 
     @Test
-    void encodeTranslationTableTest() {
-        BitSequence encodedTranslationTable = huffmanTranslationTableEncodingService.encodeTranslationTable(translationTable);
-        System.out.println(encodedTranslationTable);
-        int expectedBitSequenceLength = 32 + 32;
-        expectedBitSequenceLength += 8 * translationTable.getTotalNumberOfSymbols();
-        expectedBitSequenceLength += translationTable.getTotalNumberOfBitsForEncodedAlphabet();
-        assertEquals(expectedBitSequenceLength, encodedTranslationTable.size());
+    void generatePriorityQueueTest() {
+        PriorityQueue<HuffmanTranslationTableGenerationService.HuffmanTreeNode> priorityQueue = 
+            huffmanTranslationTableGenerationService.generatePriorityQueue(exampleSymbolTable);
+        assertEquals(4, priorityQueue.size());
+        assertEquals(1, priorityQueue.poll().getOccurrency());
+        assertEquals(1, priorityQueue.poll().getOccurrency());
+        assertEquals(2, priorityQueue.poll().getOccurrency());
+        assertEquals(3, priorityQueue.poll().getOccurrency());
+    }
+
+    @Test
+    void generateHuffmanTreeTest() {
+        HuffmanTranslationTableGenerationService.HuffmanTreeNode root = 
+            huffmanTranslationTableGenerationService.generateHuffmanTree(exampleSymbolTable);
+        assertEquals(7, root.getOccurrency());
+    }
+
+    @Test
+    void fillHuffmanTranslationTableFromHuffmanTreeTest() {
+        HuffmanTranslationTableGenerationService.HuffmanTreeNode root = 
+            huffmanTranslationTableGenerationService.generateHuffmanTree(exampleSymbolTable);
+        BitSequence emptyPrefixUsedForRecursionInit = new BitSequence();
+        Map<Byte, BitSequence> translationMapToBeFilled = new HashMap<>();
+        huffmanTranslationTableGenerationService.fillHuffmanTranslationTableFromHuffmanTree(
+            translationMapToBeFilled, root, emptyPrefixUsedForRecursionInit
+        );
+        assertEquals(4, translationMapToBeFilled.size());
+        long characterAencodeLength = translationMapToBeFilled.get((byte) characterA).toString().length();
+        long characterBencodeLength = translationMapToBeFilled.get((byte) characterB).toString().length();
+        assertTrue(characterAencodeLength < characterBencodeLength);
     }
 }
